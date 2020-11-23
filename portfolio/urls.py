71a7c6f93f93cf12dbef03e9b7c2e8426c04d8a4
kaluawa22 <kaluawa5555@gmail.com>
2020-11-22 22:42:31 -0600

    inital commit

---
+++ portfolio/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    # url(r'^projects/$', views.projects, name='projects'),
+    url(r'^resume/$', views.resume, name='resume'),
+    url(r'^contact/$', views.contact, name='contact'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ portfolio/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    context = {'classhome': "nav-current"}
+    return render(request, 'portfolio/index.html', context)
+
+def resume(request):
+    context = {'classhome': "nav-current"}
+    return render(request, 'portfolio/resume.html', context)
+def contact(request):
+    context = {'classhome': "nav-current"}
+    return render(request, 'portfolio/contact.html', context)
+
+
+
+
+# def projects(request):
+#     context = {'classhome': "nav-current"}
+#     return render(request, 'portfolio/projects.html', context)
